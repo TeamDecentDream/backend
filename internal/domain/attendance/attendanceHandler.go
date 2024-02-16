4b595070907ecc9697aa1bd85f2cbf75ab8fc6ec
GRyu1 <heliotrop1024@gmail.com>
2024-02-16 21:16:26 +0900

    출퇴근 기능

--- internal/domain/attendance/attendanceHandler.go
+++ internal/domain/attendance/attendanceHandler.go
@@ -9,11 +9,27 @@ import (
 )
 
 func EnterHandler(c *gin.Context) {
-
+	token := c.GetHeader("Authorization")
+	id, _, _, _, _ := jwt.AccessTokenVerifier(token)
+	timeLog, err := enter(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"state": timeLog.Format("2006-01-02 15:04:05"),
+	})
 }
 
 func LeaveHandler(c *gin.Context) {
-
+	token := c.GetHeader("Authorization")
+	id, _, _, _, _ := jwt.AccessTokenVerifier(token)
+	err := leave(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{})
 }
 
 func GetWorkStateHandler(c *gin.Context) {
--- internal/domain/attendance/attendanceRepository.go
+++ internal/domain/attendance/attendanceRepository.go
@@ -11,7 +11,7 @@ func GetWorkState(memberId int) (*time.Time, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	today := time.Now().In(loc).Format("2006-01-02")
 	query := "SELECT * FROM attendance WHERE member_id = ? AND DATE(enter_time) = ? AND leave_time is null order by enter_time desc"
 
@@ -25,4 +25,38 @@ func GetWorkState(memberId int) (*time.Time, error) {
 	}
 
 	return &enterTime, nil
+}
+
+func enter(memberId int) (*time.Time, error) {
+	var timelog time.Time
+	loc, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		return nil, err
+	}
+
+	query := "INSERT INTO attendance (member_id) VALUES (?)"
+	_, err = db.MyDb.Exec(query, memberId)
+	if err != nil {
+		return nil, err
+	}
+	timelog = time.Now().In(loc)
+	return &timelog, nil
+}
+
+func leave(memberId int) error {
+	loc, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		return err
+	}
+
+	now := time.Now().In(loc)
+	today := now.Format("2006-01-02")
+
+	query := "UPDATE attendance SET leave_time = ? WHERE member_id = ? AND DATE(enter_time) = ? AND leave_time IS NULL"
+
+	_, err = db.MyDb.Exec(query, now, memberId, today)
+	if err != nil {
+		return err
+	}
+	return nil
 }
